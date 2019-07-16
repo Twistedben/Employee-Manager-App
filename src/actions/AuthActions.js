98e4975b2f9444de0c7ff75aff0c7233a474f582
Benjamin Broestl <Bensgamingmail@yahoo.com>
2019-07-16 19:42:41 -0400

    Added picker and employee create. have yet to link up firebase DB

---
+++ src/actions/AuthActions.js
@@ -0,0 +1,46 @@
+import { LOGIN_USER, EMAIL_CHANGED, PASSWORD_CHANGED, LOGIN_USER_SUCCESS, LOGIN_USER_FAIL } from './types';
+import firebase from 'firebase';
+import { Actions } from 'react-native-router-flux';
+
+export const emailChanged = (text) => {
+  return {
+    type: EMAIL_CHANGED,
+    payload: text
+  };
+};
+
+export const passwordChanged = (text) => {
+  return {
+    type: PASSWORD_CHANGED,
+    payload: text
+  };
+};
+
+export const loginUser = ({email, password}) => {
+  return (dispatch) => {
+    dispatch({ type: LOGIN_USER });
+    firebase.auth().signInWithEmailAndPassword(email, password)
+      .then(user => loginUserSuccess(dispatch, user))
+      .catch((error) => {
+        console.log(error);
+
+        firebase.auth().createUserWithEmailAndPassword(email, password)
+          .then(user => loginUserSuccess(dispatch, user))
+          .catch(() => loginUserFail(dispatch));
+      });
+  };
+};
+
+const loginUserFail = (dispatch) => {
+  dispatch({ type: LOGIN_USER_FAIL });
+};
+
+// HElper method to clean up above promises to pass a success and navigate us to key main scene, which is employeelist
+const loginUserSuccess = (dispatch, user) => {
+  dispatch({
+    type: LOGIN_USER_SUCCESS,
+    payload: user
+  });
+
+  Actions.main();
+};
---
+++ src/components/EmployeeList.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+
+class EmployeeList extends Component {
+  render() {
+    return (
+      <View>
+        <Text>Employee LIst</Text>
+        <Text>Employee LIst</Text>
+        <Text>Employee LIst</Text>
+        <Text>Employee LIst</Text>
+        <Text>Employee LIst</Text>
+      </View>
+    );
+  }
+}
+
+export default EmployeeList;
